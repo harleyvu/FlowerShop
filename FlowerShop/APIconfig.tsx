2457d4322690ffde55c4dd7a28c10d726af521bd
TranGiaPhu2004 <trangiaphuc0502@gmail.com>
2025-10-12 13:48:16 +0700

    Main Flow

---
+++ FlowerShop/APIconfig.tsx
@@ -0,0 +1,8 @@
+// apiConfig.ts
+// 👉 chỉ cần sửa IP hoặc port ở đây, không cần động tới các file khác
+
+const SERVER_IP = "192.168.1.7"; // IP máy backend
+const SERVER_PORT = "5098";       // Cổng đang chạy BE
+
+// Gộp thành base URL cho API
+export const API_BASE_URL = `http://${SERVER_IP}:${SERVER_PORT}/api`;

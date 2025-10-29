03f959b3665d82296b9b7dbfc263a0311198e6b9
TranGiaPhu2004 <trangiaphuc0502@gmail.com>
2025-10-29 12:46:30 +0700

    update api

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,12 +1,12 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { Platform } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import type { Flower } from "../types/flower";
 
 const ENV_BASE = process.env.EXPO_PUBLIC_API_BASE_URL?.replace(/\/+$/, "");
 const BASE_URL =
   ENV_BASE ||
-  (Platform.OS === "android" ? "http://10.0.2.2:5098" : "http://127.0.0.1:5098");
+  (Platform.OS === "android" ? "http://10.87.18.179:5098" : "http://10.87.18.179:5098");
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
